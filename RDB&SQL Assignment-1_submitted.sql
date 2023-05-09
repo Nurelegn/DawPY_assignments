88aeaa7a78ae34c813564e3d355be5bb854ae8e1
Nurelegn <121893871+Nurelegn@users.noreply.github.com>
2023-05-09 10:48:39 +0200

    Add files via upload

---
+++ RDB&SQL Assignment-1_submitted.sql
@@ -0,0 +1,4 @@
+﻿Msg 4902, Level 16, State 1, Line 74
+Cannot find the object "Comp_Supp" because it does not exist or you do not have permissions.
+
+Completion time: 2023-05-09T10:39:13.6733378+02:00
